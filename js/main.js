5ae2148289fce36bda36e94cc4d33c60c97eb68f
gtekle <teklegy@gmail.com>
2021-12-17 16:23:05 +0300

    Hide and seek exercise

--- js/main.js
+++ js/main.js
@@ -92,16 +92,16 @@ projects.forEach((project, index) => {
             />
           </div>
           <div class="project-detail">
-            <h3 class="project-title">${project.title}</h3>
+            <h3 class="project-title">${projects.title}</h3>
             <ul class="project-metadata">
-              <li class="client">${project.client}</li>
+              <li class="client">${projects.client}</li>
               <li><span class="counter"></span></li>
               <li class="role">${project.role}</li>
               <li><span class="counter"></span></li>
               <li class="year">${project.year}</li>
             </ul>
             <p class="project-description">
-              ${project.description}
+              ${projects.description}
             </p>
             <ul class="languages project-technologies">
             </ul>
